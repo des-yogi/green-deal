9c93539a00a38383c3cba9e1117e3469b7cac92a
des-yogi <nuevo-design@mail.ru>
2023-08-24 20:43:22 +0300

    index in progress (copyright block left)

--- src/blocks/events-slider/events-slider.js
+++ src/blocks/events-slider/events-slider.js
@@ -1,4 +1,28 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  const swiper = new Swiper('.events-slider__container', {
+    speed: 400,
+    spaceBetween: 40,
+     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 16
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 32
+      },
+      1280: {
+        slidesPerView: 3,
+        spaceBetween: 40
+      }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+}());
