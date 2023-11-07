287a46683e890685ae605af09b6da5734f45da08
des-yogi <nuevo-design@mail.ru>
2023-11-07 17:36:30 +0200

    slider fixed

--- src/blocks/speakers-slider/speakers-slider.js
+++ src/blocks/speakers-slider/speakers-slider.js
@@ -1,4 +1,32 @@
-// document.addEventListener('DOMContentLoaded', function(){});
-// (function(){
-// код
-// }());
+(function(){
+  const speakers = new Swiper('.speakers-slider__container', {
+    speed: 400,
+    spaceBetween: 40,
+     breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 16
+      },
+      768: {
+        slidesPerView: 2,
+        grid: {
+          fill: 'row',
+          rows: 2,
+        },
+        spaceBetween: 32
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 40
+      }
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        type: "progressbar",
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
+}());
