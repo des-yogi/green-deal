f2da0694cef344c3d5d29f97385d87a2274f04e7
Igor <des.yogi@gmail.com>
2025-12-01 12:50:40 +0200

    fix problem with img (float left) and neighboring ul/ol

--- src/blocks/events-slider/events-slider.js
+++ src/blocks/events-slider/events-slider.js
@@ -17,12 +17,12 @@
       }
     },
     pagination: {
-        el: ".swiper-pagination",
-        type: "progressbar",
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
+      el: ".swiper-pagination",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 }());
